4f73f20594ee44ff0c2de2c05b5d36db6aaad7c4
hehe <bifire@126.com>
2019-01-22 17:53:37 +0800

    错误进行修改

--- ch01_ex01.py
+++ ch01_ex01.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     width = 72
     dotted_line = '-' * width
     print(dotted_line)
-    print("\033[lm" + "Attack of The Orcs V0.0.1:" + "\033[0m")
+    print("\033[1m" + "Attack of The Orcs V0.0.1:" + "\033[0m")
